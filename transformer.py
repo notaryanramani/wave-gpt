e30c41985be639dc1dc733c6dac36f510b78148e
Aryan Ramani <aryanramani67@gmail.com>
2024-07-04 17:52:42 +0530

    annotations and GELU activation function

--- transformer.py
+++ transformer.py
@@ -22,7 +22,7 @@ class Head(nn.Module):
         self.v = nn.Linear(n_embd, head_size)
         self.dropout = dropout
 
-    def forward(self, x) -> torch.Tensor:
+    def forward(self, x:torch.Tensor) -> torch.Tensor:
         B, T, C = x.size()
 
         q = self.q(x)
@@ -34,7 +34,7 @@ class Head(nn.Module):
 
 
 class MultiHeadAttention(nn.Module):
-    def __init__(self, n_heads, n_embd:int = params.n_embd, dropout:float = params.dropout):
+    def __init__(self, n_heads:int, n_embd:int = params.n_embd, dropout:float = params.dropout):
         super().__init__()
         assert n_embd % n_heads == 0, "n_heads should be divisible by n_embd"
         head_size = n_embd // n_heads
@@ -42,7 +42,7 @@ class MultiHeadAttention(nn.Module):
         self.proj(n_embd, n_embd)
         self.dropout = nn.Dropout(dropout)
 
-    def forward(self, x) -> torch.Tensor:
+    def forward(self, x:torch.Tensor) -> torch.Tensor:
         x = torch.cat([h(x) for h in self.heads], dim=-1)
         x = self.proj(x)
         x = self.dropout(x)
@@ -50,22 +50,22 @@ class MultiHeadAttention(nn.Module):
 
 
 class FeedForward(nn.Module):
-    def __init__(self, n_embd = params.n_embd):
+    def __init__(self, n_embd:int = params.n_embd):
         super().__init__()
         self.net = nn.Sequential(
             nn.Linear(n_embd, 4 * n_embd),
-            nn.ReLU(),
+            nn.GELU(),
             nn.Linear(4 * n_embd, n_embd),
             nn.Dropout(params.dropout)
         )
 
-    def forward(self, x) -> torch.Tensor:
+    def forward(self, x:torch.Tensor) -> torch.Tensor:
         out = self.net(x)
         return out
 
 
 class Decoder(nn.Module):
-    def __init__(self, n_heads, n_embd = params.n_embd, dropout = params.dropout):
+    def __init__(self, n_heads:int, n_embd:int = params.n_embd, dropout:float = params.dropout):
         super().__init__()
         self.mha = MultiHeadAttention(n_heads, n_embd, dropout)
         self.ffn = FeedForward(n_embd)
@@ -73,7 +73,7 @@ class Decoder(nn.Module):
         self.ln2 = nn.LayerNorm(n_embd)
         self.dropout = nn.Dropout(dropout)
 
-    def forward(self, x) -> torch.Tensor:
+    def forward(self, x:torch.Tensor) -> torch.Tensor:
         x = self.ln1(x)
         x = x + self.mha(x)
         x = self.ln2(x)
