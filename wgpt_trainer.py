f5e877840b7cdf32ddb92f2786e90a0bde3027e9
Aryan Ramani <aryanramani67@gmail.com>
2024-08-12 18:43:14 +0530

    improved main script

--- wgpt_trainer.py
+++ wgpt_trainer.py
@@ -3,11 +3,11 @@ from src import WaveGPT, OpenWebText, ModelHyperParams
 from dataclasses import dataclass
 import tiktoken
 from tqdm import tqdm
-import warnings
 from datetime import datetime
 import os
-from sklearn.metrics import accuracy_score
-warnings.filterwarnings('ignore')
+import time
+
+torch.set_float32_matmul_precision('high')
 
 
 params = ModelHyperParams()
@@ -18,8 +18,7 @@ os.makedirs('artifacts', exist_ok=True)
 
 @dataclass
 class TrainParams:
-    epochs:int = 4
-    eval_step:int = 80
+    epochs:int = 10
     learning_rate:float = 3e-4
 
 train_params = TrainParams()
@@ -30,15 +29,18 @@ data_file_path = 'data/data.txt'
 train_dataset = OpenWebText(data_file_path, split='train')
 val_dataset = OpenWebText(data_file_path, split = 'val')
 
+BATCH_TOKENS = train_dataset.block_size * 32
 
 # model initialization
 m = WaveGPT(vocab_size=tokenizer.n_vocab)
 opt = torch.optim.AdamW(m.parameters(), lr=train_params.learning_rate)
 m.to(params.device)
+m = torch.compile(m)
+torch.cuda.synchronize()
 print(f'model has {sum(p.numel() for p in m.parameters() if p.requires_grad)} parameters')
 
 
-PATH = 'artifacts\wavegpt_e_20_07_24_cp6.pth'
+PATH = 'artifacts\wavegpt_20_07_24_cp6.pth'
 if os.path.exists(PATH):
     checkpoint = torch.load(PATH)
     m.load_state_dict(checkpoint['model'])
@@ -49,15 +51,13 @@ else:
     last_epoch = -1
 
 
-if os.path.exists(f'artifacts/training_metrics_e.pt'):
-    metrics = torch.load(f'artifacts/training_metrics_e.pt')
+if os.path.exists(f'artifacts/training_metrics.pt'):
+    metrics = torch.load(f'artifacts/training_metrics.pt')
     metrics = {key:value.tolist() for key, value in metrics.items()}
 else:
     metrics = {
         'tl': [],
         'vl': [],
-        'ta': [],
-        'va': [],
     }
 
 
@@ -71,20 +71,20 @@ for epoch in range(last_epoch + 1, train_params.epochs + last_epoch + 1):
         x, y = train_dataset[step]
         x = x.to(params.device)
         y = y.to(params.device)
-
-        logits, loss = m(x, y)
+        t1 = time.time()
+        with torch.autocast(device_type=params.device, dtype=torch.bfloat16):
+            logits, loss = m(x, y)
         opt.zero_grad()
         loss.backward()
         opt.step()
-        y_pred = torch.argmax(logits, dim=-1)
-        acc = float(accuracy_score(y.view(-1).tolist(), y_pred.view(-1).tolist()))
+        torch.cuda.synchronize()
+        t2 = time.time()
+        t3 = (t2-t1) * 1000
+        toks_per_sec = (BATCH_TOKENS) / (t2 - t1)
+
         metrics['tl'].append(loss.item())
-        metrics['ta'].append(acc)
 
-        pb.set_postfix({'loss':loss.item(), 'acc' : acc})
-
-        if step % train_params.eval_step == 0:
-            pass
+        pb.set_postfix({'loss':loss.item(), 'time' : f'{t3:.4f}ms', 'toks/s' : f'{toks_per_sec:.4f}'})
 
 
     m.eval()
@@ -99,24 +99,21 @@ for epoch in range(last_epoch + 1, train_params.epochs + last_epoch + 1):
         with torch.no_grad():
             logits, loss = m(x, y)
         y_pred = torch.argmax(logits, dim=-1)
-        acc = float(accuracy_score(y.view(-1).tolist(), y_pred.view(-1).tolist()))
+        
         metrics['vl'].append(loss.item())
-        metrics['va'].append(acc)
 
-        pb.set_postfix({'loss':loss.item(), 'acc' : acc})
+        pb.set_postfix({'loss':loss.item()})
 
-        if step % train_params.eval_step == 0:
-            pass
 
     m.train()
 
 
-    PATH = f'artifacts/wavegpt_e_{today_date}_cp{epoch}.pth'
+    PATH = f'artifacts/wavegpt_{today_date}_cp{epoch}.pth'
     torch.save({
         'epoch': epoch,
         'model' : m.state_dict(),
         'opt' : opt.state_dict()
     }, PATH)
 
-    METRICS_PATH = f'artifacts/training_metrics_e.pt'
+    METRICS_PATH = f'artifacts/training_metrics.pt'
     torch.save({key:torch.tensor(metrics[key]) for key in metrics.keys()}, METRICS_PATH)
