b6af3982f5c60e38356a75f73a746c898312690b
Aryan Ramani <aryanramani67@gmail.com>
2024-10-04 17:56:56 +0100

    New dataloading script, split into shards

--- src/data_script.py
+++ src/data_script.py
@@ -1,6 +1,9 @@
 from datasets import load_dataset
 from tqdm import tqdm
 import os
+import tiktoken
+import multiprocessing as mp
+import numpy as np
 
 def download_data(FOLDER_PATH='data/', TAKE = 500000, max_file_size=500, validation_split=0.1):
     dataset = load_dataset('Skylion007/openwebtext', streaming=True, trust_remote_code=True)
@@ -42,3 +45,43 @@ def download_data(FOLDER_PATH='data/', TAKE = 500000, max_file_size=500, validat
                 current_train_size += len(encoded_text)
         
     print('saved data')
+
+tokenizer = tiktoken.get_encoding('r50k_base')
+eot = tokenizer._special_tokens['<|endoftext|>']
+def tokenize(doc):
+        tokens = [eot] + tokenizer.encode(doc['text'])
+        token_np = np.array(tokens, dtype=np.uint16)
+        return token_np
+
+def download_shards(folderPath='data/'):
+    data = load_dataset('Skylion007/openwebtext', trust_remote_code=True, split='train')
+    shard_size = int(1e8)
+
+    os.makedirs(folderPath, exist_ok=True)
+    
+    n_procs = max(1, mp.cpu_count() // 2)
+    with mp.Pool(n_procs) as pool:
+        shard_index = 0
+        all_np_tokens = np.empty((shard_size,), dtype=np.uint16)
+        current_size = 0
+        progress_bar = None
+        for tokens in pool.imap(tokenize, data, chunksize=16):
+            if current_size + len(tokens) < shard_size:
+                all_np_tokens[current_size: current_size + len(tokens)] = tokens
+                current_size += len(tokens)
+                if progress_bar is None:
+                    progress_bar = tqdm(total=shard_size, unit="tokens", desc=f"Shard {shard_index}")
+                progress_bar.update(len(tokens))
+            else:
+                split = 'val' if shard_index == 0 else 'train'
+                remainder = shard_size - current_size
+                progress_bar.update(remainder) # type: ignore
+                all_np_tokens[current_size: current_size + remainder] = tokens[:remainder]
+                np.save(f'{folderPath}/shard_{split}_{shard_index}.npy', all_np_tokens)
+                shard_index += 1
+                progress_bar = None
+                all_np_tokens = np.empty((shard_size,), dtype=np.uint16)
+                remainder = shard_size - current_size
+                all_np_tokens[0:len(tokens)-remainder] = tokens[remainder:]
+                current_size = len(tokens)-remainder
+            
